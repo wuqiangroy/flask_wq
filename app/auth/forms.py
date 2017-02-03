755c6bf6d48af774cdc71b069a9007e3d1d10615
吴强 <wuqiangroy@live.com>
2017-02-04 01:47:00 +0800

    Add files via upload

---
+++ app/auth/forms.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+import sys
+
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class LoginForm(Form):
+    username = StringField(u'用户名', validators=[DataRequired(), Length(1, 64)])
+    password = PasswordField(u'密码', validators=[DataRequired()])
+    remember_me = BooleanField(u'保持登陆状态')
+    submit = SubmitField(u'登陆')
+
+
+class RegistrationForm(Form):
+    email = StringField(u'邮箱', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    username = StringField(u'用户名', validators=[
+        DataRequired(), Length(1, 64),Regexp('^[A-Za-z][A-za-z0-9_.]*$', 0,
+                                             u'用户名只能是字母,'
+                                             u'数字, 点或是下划线')])
+    password = PasswordField(u'密码', validators=[DataRequired(),
+                                                     EqualTo('password2',
+                                                             message=u'两次密码不匹配')])
+    password2 = PasswordField(u'再次输入密码', validators=[DataRequired()])
+    submit = SubmitField(u'注册')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(u'邮箱已被注册')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError(u'用户名已被注册')
+
+
+class ChangePasswordForm(Form):
+    old_password = PasswordField(u'旧密码', validators=[DataRequired()])
+    new_password = PasswordField(u'新密码', validators=[DataRequired(),
+                                                             EqualTo('new_password2',
+                                                                     message=u'两次密码不匹配')])
+    new_password2 = PasswordField(u'再次输入新密码', validators=[DataRequired()])
+    submit = SubmitField(u'确认更改')
+
+
+class ChangeEmailForm(Form):
+    new_email = StringField(u'新邮箱', validators=[DataRequired(), Length(1, 64),
+                                                     Email()])
+    password = PasswordField(u'请输入密码', validators=[DataRequired()])
+    subumit = SubmitField(u'确认更改')
+
+    def validata_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise AttributeError(u'邮箱已被注册！')
+
+
+class ResetPasswordForm(Form):
+    email = StringField(u'邮箱', validators=[DataRequired(), Length(1,64),
+                                                  Email()])
+    username = StringField(u'用户名', validators=[DataRequired(),
+                                                        Length(1, 64)])
+    new_password = PasswordField(u'新密码', validators=[DataRequired()])
+
+
+
+
+
---
+++ app/auth/views.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+import sys
+
+from flask import render_template, url_for, redirect, request, flash, abort
+from flask_login import login_user, login_required, logout_user, current_user
+
+from . import auth
+from .. import db
+from ..models import User
+from .forms import LoginForm, RegistrationForm, ChangePasswordForm, \
+        ChangeEmailForm, ResetPasswordForm
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.ping()
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None and user.verity_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash(u'用户名或密码错误')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash(u'您已经退出！')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+                    email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(u'现在可以登陆！')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+
+@auth.route('/change/password', methods=['get', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if current_user.verity_password(form.old_password.data):
+            current_user.password = form.new_password.data
+            db.session.add(current_user)
+            db.session.commit()
+            flash(u'密码已更改！')
+            return redirect(url_for('auth.logout'))
+        else:
+            flash(u'密码错误！')
+    return render_template('auth/change_password.html', form=form)
+
+
+@auth.route('/change/email', methods=['GET', 'POST'])
+@login_required
+def change_email():
+    form = ChangeEmailForm()
+    if form.validate_on_submit():
+        if current_user.verity_password(form.password.data):
+            current_user.email = form.new_email.data
+            db.session.add(current_user)
+            db.session.commit()
+            flash(u'邮箱已更改！')
+            return redirect(url_for('main.index'))
+    return render_template('auth/change_email.html', form=form)
+
+
+@auth.route('/reset-password', methods=['GET', 'POST'])
+def reset_password():
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        email = User.query.filter_by(form.email.data).first()
+        username = User.query.filter_by(form.username.data).first()
+        if email is not None and username is not None:
+            user = User(password=form.new_password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash(u'密码已重置！')
+            return redirect(url_for('auth.login'))
+        else:
+            flash(u'邮箱或用户名错误！')
+    return render_template('auth/reset_password.html', form=form)
+
+
