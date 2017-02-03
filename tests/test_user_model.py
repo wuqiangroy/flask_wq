1943243d3c976f128b592154d4b0d79a601835a4
吴强 <wuqiangroy@live.com>
2017-02-04 01:45:20 +0800

    Add files via upload

---
+++ tests/test_user_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+import unittest
+from app.models import User, Role, Permission, AnonymousUser
+
+
+class UserModelTestCase(unittest.TestCase):
+    def test_password_setter(self):
+        u = User(password='cat')
+        self.assertTrue(u.password_hash is not None)
+
+    def test_no_password_getter(self):
+        u = User(password='cat')
+        with self.assertRaises(AttributeError):
+            u.password
+
+    def test_password_verification(self):
+        u = User(password='cat')
+        self.assertTrue(u.verity_password('cat'))
+        self.assertFalse(u.verity_password('dog'))
+
+    def test_password_salts_are_random(self):
+        u = User(password='cat')
+        u2 = User(password='cat')
+        self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_roles_and_permissions(self):
+        Role.insert_roles()
+        u = User(email='wuqiangroy@live.com', password='cat')
+        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
+        self.assertFalse(u.can(Permission.MODERATE_COMMENTS))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
+
