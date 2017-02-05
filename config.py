df23670a260d5592a2531c874cf1b3ccf7053a58
吴强 <wuqiangroy@live.com>
2017-02-06 01:09:30 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -8,11 +8,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = 'hard to guess string'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     WQ_MAIL_SUBJECT_PREFIX = '[WQ]'
     WQ_MAIL_SENDER = 'WQ Admin<wuqiangroy@live.com>'
     WQ_ADMIN = os.environ.get('WQ_ADMIN') or 'wuqiangroy@live.com'
+    WQ_COMMENTS_PER_PAGE = 30
+    WQ_POSTS_PER_PAGE = 20
+    WQ_FOLLOWERS_PER_PAGE = 50
+    WQ_USERS_PER_PAGE = 50
+    WQ_SLOW_DB_QUERY_TIME = True
+    WQ_DB_QUERY_TIMEOUT = 0.5
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     @staticmethod
     def init_app(app):
@@ -35,11 +42,35 @@ class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    WTF_CSRF_ENABLED = False
 
 
 class ProductionConfig(Config):
     SQLQLCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # 发送错误信息
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.WQ_MAIL_SENDER,
+            toaddr=[cls.WQ_ADMIN],
+            subject=cls.WQ_MAIL_SUBJECT_PREFIX + ' Application Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setlevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 config = {
     'development': DevelopmentConfig,
@@ -47,5 +78,4 @@ config = {
     'production': ProductionConfig,
 
     'default': DevelopmentConfig,
-
 }
--- manage.py
+++ manage.py
@@ -2,9 +2,10 @@
 # _*_ coding:utf-8 _*_
 
 import os
+import sys
 
 from app import create_app, db
-from app.models import User, Role, Permission
+from app.models import User, Role, Permission, Follow, Comment
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -12,21 +13,66 @@ app = create_app(os.getenv('WQ_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+COV = None
+if os.environ.get('WQ_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role,
-                Permission=Permission)
+    return dict(app=app, db=db, User=User, Role=Role, Follow=Follow,
+                Comment=Comment, Permission=Permission)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def test():
+def test(coverage=False):
     """Run the unit tests."""
+    if coverage and not os.environ.get('WQ_COVERAGE'):
+        import sys
+        os.environ['WQ_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print(u'覆盖测试总结：')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'temp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
+
+
+@manager.command
+def profile(length=25, profile_dir=None):
+        """Start the application under the code profile."""
+        from werkzeug.contrib.profiler import ProfilerMiddleware
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                          profile_dir=profile_dir)
+        app.run()
+
+
+@manager.command
+def deploy():
+    """Run deployment tasks."""
+    from flask_migrate import upgrade
+    from app.models import Role, User
+
+    # 迁移数据
+    upgrade()
+
+    # 创建用户角色
+    Role.insert_roles()
 
 if __name__ == '__main__':
     manager.run()
