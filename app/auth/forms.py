ae4ed9a5c3ab54d0b8c58525820011976564302b
QiangWu <wuqiangroy@live.com>
2017-02-08 21:41:31 +0800

    Add files via upload

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,60 +14,61 @@ sys.setdefaultencoding('utf-8')
 
 
 class LoginForm(Form):
-    username = StringField('用户名', validators=[DataRequired(), Length(1, 64)])
-    password = PasswordField('密码', validators=[DataRequired()])
-    remember_me = BooleanField('保持登陆状态')
-    submit = SubmitField('登陆')
+    username = StringField(u'用户名', validators=[DataRequired(), Length(1, 64)])
+    password = PasswordField(u'密码', validators=[DataRequired()])
+    remember_me = BooleanField(u'保持登陆状态')
+    submit = SubmitField(u'登陆')
 
 
 class RegistrationForm(Form):
-    email = StringField('邮箱', validators=[DataRequired(), Length(1, 64),
+    email = StringField(u'邮箱', validators=[DataRequired(), Length(1, 64),
                                              Email()])
-    username = StringField('用户名', validators=[
+    username = StringField(u'用户名', validators=[
         DataRequired(), Length(1, 64),Regexp('^[A-Za-z][A-za-z0-9_.]*$', 0,
-                                             '用户名只能是字母,'
-                                             '数字, 点或是下划线')])
-    password = PasswordField('密码', validators=[DataRequired(),
+                                             u'用户名只能是字母,'
+                                             u'数字, 点或是下划线')])
+    password = PasswordField(u'密码', validators=[DataRequired(),
                                                      EqualTo('password2',
-                                                             message='两次密码不匹配')])
-    password2 = PasswordField('再次输入密码', validators=[DataRequired()])
-    submit = SubmitField('注册')
+                                                             message=u'两次密码不匹配')])
+    password2 = PasswordField(u'再次输入密码', validators=[DataRequired()])
+    submit = SubmitField(u'注册')
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
-            raise ValidationError('邮箱已被注册')
+            raise ValidationError(u'邮箱已被注册')
 
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
-            raise ValidationError('用户名已被注册')
+            raise ValidationError(u'用户名已被注册')
 
 
 class ChangePasswordForm(Form):
-    old_password = PasswordField('旧密码', validators=[DataRequired()])
-    new_password = PasswordField('新密码', validators=[DataRequired(),
+    old_password = PasswordField(u'旧密码', validators=[DataRequired()])
+    new_password = PasswordField(u'新密码', validators=[DataRequired(),
                                                              EqualTo('new_password2',
-                                                                     message='两次密码不匹配')])
-    new_password2 = PasswordField('再次输入新密码', validators=[DataRequired()])
-    submit = SubmitField('确认更改')
+                                                                     message=u'两次密码不匹配')])
+    new_password2 = PasswordField(u'再次输入新密码', validators=[DataRequired()])
+    submit = SubmitField(u'确认更改')
 
 
 class ChangeEmailForm(Form):
-    new_email = StringField('新邮箱', validators=[DataRequired(), Length(1, 64),
+    new_email = StringField(u'新邮箱', validators=[DataRequired(), Length(1, 64),
                                                      Email()])
-    password = PasswordField('请输入密码', validators=[DataRequired()])
-    subumit = SubmitField('确认更改')
+    password = PasswordField(u'请输入密码', validators=[DataRequired()])
+    subumit = SubmitField(u'确认更改')
 
     def validata_email(self, field):
         if User.query.filter_by(email=field.data).first():
-            raise AttributeError('邮箱已被注册！')
+            raise AttributeError(u'邮箱已被注册！')
 
 
 class ResetPasswordForm(Form):
-    email = StringField('邮箱', validators=[DataRequired(), Length(1,64),
+    email = StringField(u'邮箱', validators=[DataRequired(), Length(1,64),
                                                   Email()])
-    username = StringField('用户名', validators=[DataRequired(),
+    username = StringField(u'用户名', validators=[DataRequired(),
                                                         Length(1, 64)])
-    new_password = PasswordField('新密码', validators=[DataRequired()])
+    new_password = PasswordField(u'新密码', validators=[DataRequired()])
+    submit = SubmitField(u'确认重置')
 
 
 
