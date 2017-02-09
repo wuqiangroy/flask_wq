06d100741171bf189af4438c714a7868d49a3153
QiangWu <wuqiangroy@live.com>
2017-02-09 23:07:33 +0800

    Add files via upload

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,61 +4,73 @@
 import sys
 
 from flask_wtf import Form
-from flask_pagedown.fields import PageDownField
-from wtforms import StringField, SubmitField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Length, Email, Regexp, ValidationError
-from ..models import Role, User
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-class NameForm(Form):
-    name = StringField("what's your name?", validators=[DataRequired()])
-    submit = SubmitField('submit')
+class LoginForm(Form):
+    username = StringField(u'用户名', validators=[DataRequired(), Length(1, 64)])
+    password = PasswordField(u'密码', validators=[DataRequired()])
+    remember_me = BooleanField(u'保持登陆状态')
+    submit = SubmitField(u'登陆')
 
 
-class EditProfileForm(Form):
-    name = StringField(u'真实姓名', validators=[Length(0, 64)])
-    location = StringField(u'住址', validators=[Length(0, 64)])
-    about_me = TextAreaField(u'一句话介绍你自己')
-    submit = SubmitField(u'提交')
-
-
-class EditProfileAdminForm(Form):
+class RegistrationForm(Form):
     email = StringField(u'邮箱', validators=[DataRequired(), Length(1, 64),
                                              Email()])
     username = StringField(u'用户名', validators=[
-        DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-                                              u'用户名只能由字母,'
-                                              u'数字,点或下划线组成')])
-    name = StringField(u'真实姓名', validators=[Length(0, 64)])
-    location = StringField(u'住址', validators=[Length(0, 64)])
-    about_me = TextAreaField(u'一句话介绍你自己')
-    submit = SubmitField(u'提交')
-
-    def __init__(self, user):
-        super(EditProfileAdminForm, self).__init__()
-        self.user = user
+        DataRequired(), Length(1, 64),Regexp('^[A-Za-z][A-za-z0-9_.]*$', 0,
+                                             u'用户名只能是字母,'
+                                             u'数字, 点或是下划线')])
+    password = PasswordField(u'密码', validators=[DataRequired(),
+                                                     EqualTo('password2',
+                                                             message=u'两次密码不匹配')])
+    password2 = PasswordField(u'再次输入密码', validators=[DataRequired()])
+    submit = SubmitField(u'注册')
 
     def validate_email(self, field):
-        if field.data != self.user.email and \
-                User.query.filter_by(email=field.data).first():
+        if User.query.filter_by(email=field.data).first():
             raise ValidationError(u'邮箱已被注册')
 
     def validate_username(self, field):
-        if field.data != self.user.username and \
-                User.query.filter_by(username=field.data).first():
+        if User.query.filter_by(username=field.data).first():
             raise ValidationError(u'用户名已被注册')
 
 
-class PostForm(Form):
-    title = StringField(u'标题', validators=[DataRequired(u"不能为空！")])
-    body = TextAreaField(u'内容', validators=[DataRequired(u"不能为空！")])
-    submit = SubmitField(u'发布')
+class ChangePasswordForm(Form):
+    old_password = PasswordField(u'旧密码', validators=[DataRequired()])
+    new_password = PasswordField(u'新密码', validators=[DataRequired(),
+                                                             EqualTo('new_password2',
+                                                                     message=u'两次密码不匹配')])
+    new_password2 = PasswordField(u'再次输入新密码', validators=[DataRequired()])
+    submit = SubmitField(u'确认更改')
 
 
-class CommentForm(Form):
-    body = StringField('', validators=[DataRequired()])
-    submit = SubmitField(u'发表回复')
+class ChangeEmailForm(Form):
+    new_email = StringField(u'新邮箱', validators=[DataRequired(), Length(1, 64),
+                                                     Email()])
+    password = PasswordField(u'请输入密码', validators=[DataRequired()])
+    subumit = SubmitField(u'确认更改')
+
+    def validata_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise AttributeError(u'邮箱已被注册！')
+
+
+class ResetPasswordForm(Form):
+    email = StringField(u'邮箱', validators=[DataRequired(), Length(1,64),
+                                                  Email()])
+    username = StringField(u'用户名', validators=[DataRequired(),
+                                                        Length(1, 64)])
+    new_password = PasswordField(u'新密码', validators=[DataRequired()])
+    submit = SubmitField(u'确认重置')
+
+
+
+
 
